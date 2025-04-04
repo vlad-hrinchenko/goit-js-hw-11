8f70774c514aa811bba88c702de7045b9cb8964c
Vlad Hrinchenko <vladuslavhrinchenko@gmail.com>
2025-04-04 12:34:46 +0300

    change name files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { getImagesByQuery } from "./js/pixabay-api.js";
-import { createGallery, clearGallery, showLoader, hideLoader } from "./js/render-function.js";
+import { createGallery, clearGallery, showLoader, hideLoader } from "./js/render-functions.js";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
